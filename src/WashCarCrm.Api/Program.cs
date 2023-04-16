a805da5dd689988a7495f76927ae49334d68c5c9
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-04-16 17:32:47 +0500

    CODE RUB: Update Program

--- src/WashCarCrm.Api/Program.cs
+++ src/WashCarCrm.Api/Program.cs
@@ -59,7 +59,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => 
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger v1");
+        c.RoutePrefix = String.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
--- src/WashCarCrm.Domain/UserRole.cs
+++ src/WashCarCrm.Domain/UserRole.cs
@@ -1,5 +1,7 @@
+using System.Text.Json.Serialization;
 namespace WashCarCrm.Domain
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum UserRole
     {
         Admin,
